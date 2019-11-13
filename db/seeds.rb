ae5bfacabadafff14a5e9aadd9317969d3a4c9de
balthazar lebret <balthazarlb@gmail.com>
2019-11-13 16:41:24 +0100

    exo rails yeah

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+Restaurant.create(name: " Pluto ", address: "150 rue Sainte Catherine, Bordeaux ", category: "japanese")
+Restaurant.create(name: " toplu ", address: "18 rue BOBO, Bordeaux ", category: "french")
+Restaurant.create(name: " nokogiri ", address: "50 avenue Sainte, Paris ", category: "chinese")
+Restaurant.create(name: " bobobino ", address: "2 rue Pierre Michel, Nimes ", category: "belgian")
+Restaurant.create(name: " Hasta Luego ", address: "56 rue du Temple, Youlouse ", category: "italian")
+
+Restaurant.all.each do |p|
+  puts "Created #{p.name} "
+end
